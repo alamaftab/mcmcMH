d60752c3c98fbfbffcb13969bd13ef018be33f94
unknown <aalam0@USHOFLT320975.kih.kmart.com>
2013-09-26 14:07:25 -0500

    move all to log scale

--- bin/mcmcMH2.R
+++ bin/mcmcMH2.R
@@ -27,24 +27,29 @@ SMAVolume20 <- SMA(histDataOrd[,7], 20)
 SMAPrice20 <- SMA(histDataOrd[,6], 20)
 # get next day price array and add it to main dataframe
 OneDayAheadP <- histDataOrd[(2:histDataRowCount),6]
-OneDayAheadP[histDataRowCount] = 0.0
+OneDayAheadP[histDataRowCount] = OneDayAheadP[histDataRowCount - 1]
+# get previous day price array and add it to main dataframe
+OneDayBackP <- histDataOrd[(1:histDataRowCount-1),6]
+OneDayBackP = c(0.0,OneDayBackP )
 
-histDataOrd1 <-cbind(histDataOrd[,6], histDataOrd[,7],SMAPrice20, SMAVolume20,OneDayAheadP)
+histDataOrd1 <-cbind(histDataOrd[,6], histDataOrd[,7],SMAPrice20, SMAVolume20,OneDayBackP, OneDayAheadP)
 Dt <-as.integer(histDataOrd[,2])
 histDataOrd1 <-cbind(Dt, histDataOrd1)
 
 HDOrdLn <- data.frame(Dt=histDataOrd1[,1],PLn=log(histDataOrd1[,2]), VLn=log(histDataOrd1[,3]),
-            PSmaLn = log(histDataOrd1[,4]) , VSmaLn =log(histDataOrd1[,5]) , AheadP = histDataOrd1[,6])
+           PSmaLn = log(histDataOrd1[,4]) , VSmaLn =log(histDataOrd1[,5]) , BackPLn1 = log(histDataOrd1[,6]) ,
+           AheadP = log(histDataOrd1[,7]))
 
 
-posterior1 <- MCMCregress( AheadP ~ PLn + VLn + PSmaLn + VSmaLn , data=HDOrdLn)
+posterior1 <- MCMCregress( AheadP ~ PLn + VLn + PSmaLn + VSmaLn + BackPLn1 , data=HDOrdLn)
 
 mcmcSummary <-summary(posterior1)
 
 attach(HDOrdLn)
 
-  prnew = ( mcmcSummary[[1]][[2]]*PLn +    mcmcSummary[[1]][[3]]*VLn  +  
-            mcmcSummary[[1]][[4]]*PSmaLn  + mcmcSummary[[1]][[5]]*VSmaLn   ) +  mcmcSummary[[1]][[1]]  
+  prnew = (( mcmcSummary[[1]][[2]]*PLn +    mcmcSummary[[1]][[3]]*VLn  +  
+            mcmcSummary[[1]][[4]]*PSmaLn  + mcmcSummary[[1]][[5]]*VSmaLn +
+            mcmcSummary[[1]][[6]]*BackPLn1  ) +  mcmcSummary[[1]][[1]] ) 
 
   kk <- cbind(Dt,prnew, AheadP )
 
--- bin/mcmcMHSimulation2.R
+++ bin/mcmcMHSimulation2.R
@@ -8,21 +8,16 @@ attach(HDOrdLn)
 
 for ( i in 1:10)
 {
-    CofIncept = rnorm(1,mcmcSummary[[1]][[1]] ,mcmcSummary[[1]][[7]]/1000)
-    CofPLn = rnorm(1, mcmcSummary[[1]][[2]] ,mcmcSummary[[1]][[8]]/100)
-    CofVLn = rnorm(1, mcmcSummary[[1]][[3]] ,mcmcSummary[[1]][[9]]/100)
-    CofPSmaLn = rnorm(1, mcmcSummary[[1]][[4]] ,mcmcSummary[[1]][[10]]/100)
-    #CofPSmaLn = mcmcSummary[[1]][[4]] 
-    CofVSmaLn = rnorm(1, mcmcSummary[[1]][[5]] ,mcmcSummary[[1]][[11]]/100)
-
- 
   
-  for ( j in 50:60)
+  for ( j in 2:150)
    {
+
+    
      tmpPLn = PLn[j]
      tmpVLn = VLn[j]
      tmpPSmaLn = PSmaLn[j]
      tmpVSmaLn = VSmaLn[j]
+     tmpBackPLn1 = BackPLn1[j]
      tmpDt = Dt[j]
      tmpAheadP = AheadP[j]
 
@@ -30,11 +25,19 @@ for ( i in 1:10)
      
      for ( k in 1:10)
      {
-       prdictVal = 0
-       prdictVal = ( CofPLn*tmpPLn +    CofVLn*tmpVLn  + CofPSmaLn*tmpPSmaLn  + CofVSmaLn*tmpVSmaLn ) +  CofIncept  
        
-       print(prdictVal)
-       tmpPLn = log(prdictVal)
+       CofIncept = rnorm(1,mcmcSummary[[1]][[1]] ,mcmcSummary[[1]][[8]]/100)
+       CofPLn = rnorm(1, mcmcSummary[[1]][[2]] ,mcmcSummary[[1]][[9]]/100)
+       CofVLn = rnorm(1, mcmcSummary[[1]][[3]] ,mcmcSummary[[1]][[10]]/100)
+       CofPSmaLn = rnorm(1, mcmcSummary[[1]][[4]] ,mcmcSummary[[1]][[11]]/100)
+       CofVSmaLn = rnorm(1, mcmcSummary[[1]][[5]] ,mcmcSummary[[1]][[12]]/100)
+       CofBackPLn1 = rnorm(1, mcmcSummary[[1]][[6]] ,mcmcSummary[[1]][[13]]/100)
+       
+       prdictVal = 0
+       prdictVal = (( CofPLn*tmpPLn +    CofVLn*tmpVLn  + CofPSmaLn*tmpPSmaLn  + CofVSmaLn*tmpVSmaLn + CofBackPLn1*tmpBackPLn1 ) +  CofIncept ) 
+       
+       print(exp(prdictVal))
+       tmpPLn = prdictVal
        tmpDt = Dt[j+k]
        tmpAheadP = AheadP[j+k]
 
@@ -48,9 +51,12 @@ for ( i in 1:10)
 detach(HDOrdLn)
 
 a <-strptime(dataCollect[,1], "%Y%m%d")
-v <- data.frame(a,dataCollect[,2])
+v <- data.frame(a,exp(dataCollect[,2]))
+v1 <- data.frame(a,exp(dataCollect[,3]))
 
-plot(v)
+plot(v, ylim = c(110,140))
+lines(v1, type="o", pch=22, lty=2, col="red")
+
 
 #abline(lm(dd[,2] ~ dd[,1]), lty=2)
 
